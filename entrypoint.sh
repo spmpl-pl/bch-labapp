ce4a1b735f30cb5492528a52a06fbf1f2a60a223
Chmielewski Bartosz <bartosz@spm.pl>
2026-05-13 21:08:33 +0200

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ if [ -z "$SECRET_KEY" ]; then
   export SECRET_KEY=$(python -c "import secrets; print(secrets.token_hex(32))")
 fi
 
-exec gunicorn -b 0.0.0.0:8000 app:app
+exec gunicorn -b 0.0.0.0:8000 --workers 3 --worker-class gthread --threads 4  app:app
